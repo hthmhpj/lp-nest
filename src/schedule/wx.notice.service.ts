d09863e7ec4dc6180800c8409c7622a6a0f5f841
tracy <lx_hthmh@163.com>
2021-08-04 22:47:44 +0800

    -

--- src/schedule/wx.notice.service.ts
+++ src/schedule/wx.notice.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class WxNotice {
   constructor(private httpService: HttpService) {}
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron("0 40 09 * * 1-5")
   async weeklyNotice() {
     const res = await this.httpService.post(
       'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d495e6ea-b6dc-4e1c-9ee6-46729a2c12b5',
       {
         msgtype: 'text',
         text: {
-          content: '写周报',
+          content: '写日报',
           mentioned_list: ['@all'],
         },
       },
